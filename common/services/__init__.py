7e7fc0d9e15bfe748026ec512d848836af490dec
Valera <valera@MacBook-Pro-7.local>
2025-08-29 10:59:00 +0200

    🔧 Исправление импорта ScenarioService - добавлен в common/services

--- common/services/__init__.py
+++ common/services/__init__.py
@@ -3,9 +3,11 @@
 """
 
 from .unified import UnifiedTextService, UnifiedScenarioService, UnifiedSettingService
+from .services_package.scenario_service import ScenarioService
 
 __all__ = [
     "UnifiedTextService",
     "UnifiedScenarioService",
-    "UnifiedSettingService"
+    "UnifiedSettingService",
+    "ScenarioService"
 ]
